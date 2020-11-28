a8493b0cc1d6e220ac30d8a53e98eb1dd08746a3
seoguren <estebansologuren@gmail.com>
2020-11-27 22:35:00 -0300

    actualizacion del curso

---
+++ POO-clases/main.py
@@ -0,0 +1,30 @@
+class Coche:
+
+    color = "Rojo"
+    marca = "Ferrari"
+    modelo = "Aventador"
+    velocidad = 300
+    caballaje = 500
+    puestos = 2
+
+    def acelerar(self):
+        self.velocidad += 1
+
+    def frenar(self):
+        self.velocidad += 1
+
+    def getVelocidad(self):
+        return self.velocidad
+
+    def setColor(self, color):
+        self.color = amarill
+    
+    def getColor(self)
+    return self.color
+
+# fin definicion de la clase
+
+
+coche = Coche()
+print(coche.marca, coche.color)
+print("velocidad actual: ", coche.velocidad)
